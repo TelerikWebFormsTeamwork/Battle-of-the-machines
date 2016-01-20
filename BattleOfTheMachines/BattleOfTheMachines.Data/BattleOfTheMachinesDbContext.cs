ecf49e8a0167bc4515f348304947f4fc262e2b68
Novkirishki <novkirishki94@gmail.com>
2016-01-20 22:45:26 +0200

    Added web forms project and extracted data layer

---
+++ BattleOfTheMachines/BattleOfTheMachines.Data/BattleOfTheMachinesDbContext.cs
@@ -0,0 +1,18 @@
+﻿namespace BattleOfTheMachines.Data
+{
+    using Microsoft.AspNet.Identity.EntityFramework;
+    using Models;
+
+    public class BattleOfTheMachinesDbContext : IdentityDbContext<User>
+    {
+        public BattleOfTheMachinesDbContext()
+            : base("DefaultConnection", throwIfV1Schema: false)
+        {
+        }
+
+        public static BattleOfTheMachinesDbContext Create()
+        {
+            return new BattleOfTheMachinesDbContext();
+        }
+    }
+}
---
+++ BattleOfTheMachines/BattleOfTheMachines.WebForms/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("BattleOfTheMachines.WebForms")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("BattleOfTheMachines.WebForms")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("f13f8512-29b5-4713-824f-68466f099a8b")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
