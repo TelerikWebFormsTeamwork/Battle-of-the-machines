6b446b141881759785de2624443841aba5454403
Mihaela Encheva <dyct.ent@gmail.com>
2016-01-26 20:22:56 +0200

    404 is now handled :hatched_chick:

---
+++ BattleOfTheMachines/BattleOfTheMachines.WebForms/Errors/Oops.aspx
@@ -0,0 +1,5 @@
+﻿<%@ Page Language="C#" MasterPageFile="~/Site.Master" AutoEventWireup="true" CodeBehind="Oops.aspx.cs" Inherits="BattleOfTheMachines.WebForms.Errors.Oops" %>
+
+<asp:Content ID="BodyContent" ContentPlaceHolderID="MainContent" runat="server">
+    <div class="jumbotron"><h1>Oops...</h1><h2>Nothing here - go back! :)</h2></div>
+</asp:Content>
--- BattleOfTheMachines/BattleOfTheMachines.WebForms/Global.asax.cs
+++ BattleOfTheMachines/BattleOfTheMachines.WebForms/Global.asax.cs
@@ -19,12 +19,29 @@ namespace BattleOfTheMachines.WebForms
 
     public class Global : HttpApplication
     {
-        void Application_Start(object sender, EventArgs e)
+        public void Application_Start(object sender, EventArgs e)
         {
             // Code that runs on application startup
             DatabaseConfig.Initialize();
             RouteConfig.RegisterRoutes(RouteTable.Routes);
             BundleConfig.RegisterBundles(BundleTable.Bundles);
+        }
+
+        protected void Application_Error(object sender, EventArgs e)
+        {
+            HttpApplication application = (HttpApplication)sender;
+            Exception lastError = application.Server.GetLastError();
+            HttpException ex = lastError as HttpException;
+            string fourOhFourPage = "~/Errors/Oops.aspx";
+            // Only response to 404 errors
+            if (ex != null && ex.GetHttpCode() == 404)
+            {
+                // Clear the error in order to avoid standard handling of the error.
+                application.Server.ClearError();
+                application.Context.Handler =
+                      System.Web.UI.PageParser.GetCompiledPageInstance(fourOhFourPage,
+                      application.Server.MapPath(fourOhFourPage), application.Context);
+            }
         }
     }
 }
