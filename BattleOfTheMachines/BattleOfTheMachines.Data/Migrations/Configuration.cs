ca7099766187c7d3870026708c6e76e980f5d8b2
Novkirishki <novkirishki94@gmail.com>
2016-01-27 11:05:15 +0200

    Added admin in seed, roles are ready

--- BattleOfTheMachines/BattleOfTheMachines.Data/Migrations/Configuration.cs
+++ BattleOfTheMachines/BattleOfTheMachines.Data/Migrations/Configuration.cs
@@ -28,7 +28,30 @@ namespace BattleOfTheMachines.Data.Migrations
                 context.Roles.Add(new IdentityRole { Name = "Admin" });
                 context.SaveChanges();
             }
-            
+
+            if (!context.Users.Where(u => u.UserName == "admin@admin.com").Any())
+            {
+                var admin = new User
+                {
+                    UserName = "admin@admin.com",
+                    Email = "admin@admin.com",
+                    PasswordHash = "AJ6xY/N9kWRShbJYh8QE5fIt5Yg5IeyTFBnJ9RXAdIXUo9hiqxtVWRiYJ0XyUHVDbg==",
+                    SecurityStamp = "59bcf0ca-0367-4397-b3c1-af413e479e0c",
+                    LockoutEnabled = true,
+                };
+
+                context.Users.Add(admin);
+
+                var role = context.Roles.Where(r => r.Name == "Admin").FirstOrDefault();
+                var adminRole = new IdentityUserRole
+                {
+                    RoleId = role.Id
+                };
+
+                admin.Roles.Add(adminRole);            
+                context.SaveChanges();
+            }
+
             if (!context.GraphicsCards.Any())
             {
                 var graphicsImageBytes = File.ReadAllBytes(this.MapPath("~/Images/graphics_default.png"));
